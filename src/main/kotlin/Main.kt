79d93fdbf3801cfe7607e8c9d2a9e4a594f84652
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-30 17:14:16 +0200

    Add second mode to cli

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,16 +12,20 @@ private fun serveUser() {
 }
 
 private fun tryToServeUser() {
-    println("Usage: n")
+    println("Usage: n first/all")
     val line = readLine()!!.split(' ')
     val n = line[0].toInt()
-    printQueensResult(n)
+    val mode = line[1]
+    val first = isFirstMode(mode)
+    printQueensResult(n, first)
     serveUser()
 }
 
-private fun printQueensResult(n: Int) {
+private fun isFirstMode(mode: String) = mode[0] in arrayOf('f', 'F')
+
+private fun printQueensResult(n: Int, first: Boolean) {
     val problem = QueensProblem(n)
     val executor = BacktrackingExecutor(problem)
-    val result = executor.findFirst()
+    val result: Any? = if (first) executor.findFirst() else executor.countAll()
     println(result)
 }
