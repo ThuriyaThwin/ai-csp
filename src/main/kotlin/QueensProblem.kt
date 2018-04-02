1d066efed3a64e22c99fd579fad92b7172c9f099
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-02 14:40:44 +0200

    Add a heuristics

--- src/main/kotlin/QueensProblem.kt
+++ src/main/kotlin/QueensProblem.kt
@@ -12,24 +12,41 @@ data class QueensProblem(
         get() = board.any { it.domain.isEmpty() }
 
     override fun setVariable(variableIndex: Int, value: Int): Problem {
-        val updatedBoard = board.copy { this[variableIndex] = this[variableIndex].copy(value = value) }
+        val actualVariableIndex = actualVariableIndex(variableIndex)
+        val updatedBoard = board.copy { this[actualVariableIndex] = this[actualVariableIndex].copy(value = value) }
         return copy(board = updatedBoard)
     }
 
-    override fun domainOfVariable(variableIndex: Int): Domain = board[variableIndex].domain
+    override fun domainOfVariable(variableIndex: Int): Domain {
+        val actualVariableIndex = actualVariableIndex(variableIndex)
+        return board[actualVariableIndex].domain
+    }
+
+    private fun actualVariableIndex(variableIndex: Int) = numberOfVariables / 2 + (if (variableIndex % 2 == 0) 1 else -1) * (variableIndex + 1) / 2
 
     override fun areConstrainsSatisfied(variableIndex: Int, value: Int): Boolean {
-        for (previousColumn in 0 until variableIndex) {
-            if (board[previousColumn].value == value) return false
-            if (variableIndex - previousColumn == Math.abs(value - board[previousColumn].value)) return false
+        val actualVariableIndex = actualVariableIndex(variableIndex)
+        for (otherColumn in (0 until numberOfVariables) - actualVariableIndex) {
+            if (board[otherColumn].value != 0) {
+                if (board[otherColumn].value == value) return false
+                if (Math.abs(actualVariableIndex - otherColumn) == Math.abs(value - board[otherColumn].value)) return false
+            }
         }
         return true
     }
 
     override fun updateDomains(variableIndex: Int, value: Int): Problem {
+        val actualVariableIndex = actualVariableIndex(variableIndex)
         val newBoard = board.copy {
             var increment = 1
-            for (columnIndex in (variableIndex + 1) until numberOfVariables) {
+            for (columnIndex in (actualVariableIndex + 1) until numberOfVariables) {
+                val oldDomain = this[columnIndex].domain
+                val newDomain = oldDomain - value - (value + increment) - (value - increment)
+                this[columnIndex] = this[columnIndex].copy(domain = newDomain)
+                ++increment
+            }
+            increment = 1
+            for (columnIndex in (actualVariableIndex - 1) downTo 0) {
                 val oldDomain = this[columnIndex].domain
                 val newDomain = oldDomain - value - (value + increment) - (value - increment)
                 this[columnIndex] = this[columnIndex].copy(domain = newDomain)
