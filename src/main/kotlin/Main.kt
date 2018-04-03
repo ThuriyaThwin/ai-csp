c2f1ca6ed6f9745fa6e99864c647f50e70cf66a2
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-03 18:13:47 +0200

    Delete tornadoFx

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,19 @@
 import algorithm.BacktrackingExecutor
-import javafx.application.Application
-import javafx.scene.chart.NumberAxis
-import javafx.scene.layout.GridPane
 import problem.QueensProblem
-import tornadofx.App
-import tornadofx.View
-import tornadofx.linechart
-import tornadofx.multiseries
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    Application.launch(ChartApp::class.java, *args)
-}
-
-class ChartApp : App() {
-    override val primaryView = ChartView::class
-}
-
-class ChartView : View("Algorytm genetyczny") {
-    override val root = GridPane()
-
-    init {
-        linechart(
-            "Tytuł wykresu",
-            NumberAxis(),
-            NumberAxis()
-        ) {
-            createSymbols = false
-            multiseries("backtracking", "forward checking") {
-                for (n in 0..10) {
-                    val problem = QueensProblem(n)
-                    val backtracking = BacktrackingExecutor(problem)
-                    val backtrackingTime = measureTimeMillis {
-                        backtracking.countAll()
-                    }
-                    val forwardChecking = BacktrackingExecutor(problem)
-                    val forwardChecingTime = measureTimeMillis {
-                        forwardChecking.countAll()
-                    }
-                    backtracking.findFirst()
-                    data(n, backtrackingTime, forwardChecingTime)
-                }
-            }
+    println("N,backtracking(czas),forward checking(czas)")
+    for (n in 1..13) {
+        val problem = QueensProblem(n)
+        val backtracking = BacktrackingExecutor(problem)
+        val backtrackingTime = measureTimeMillis {
+            backtracking.countAll()
         }
+        val forwardChecking = BacktrackingExecutor(problem)
+        val forwardCheckingTime = measureTimeMillis {
+            forwardChecking.countAll()
+        }
+        println("$n,$backtrackingTime,$forwardCheckingTime")
     }
 }
