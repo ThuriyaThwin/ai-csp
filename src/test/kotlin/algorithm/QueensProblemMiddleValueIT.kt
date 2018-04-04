6dd07e5cea99c12c6352ec480096a0d21645d4e7
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-04 10:01:57 +0200

    Fix test

--- src/test/kotlin/algorithm/QueensProblemMiddleValueIT.kt
+++ src/test/kotlin/algorithm/QueensProblemMiddleValueIT.kt
@@ -1,6 +1,5 @@
 package algorithm
 
-import chooser.MiddleChooser
 import problem.Problem
 import problem.QueensProblem
 import problem.middleValueBoard
@@ -8,7 +7,7 @@ import problem.middleValueBoard
 internal class QueensProblemMiddleValueIT : CountAllAbstractTest {
 
     override val problemInitializer: (n: Int) -> Problem =
-        { QueensProblem(it, MiddleChooser(it), middleValueBoard(it)) }
+        { QueensProblem(it, board = middleValueBoard(it)) }
 
     override val allResultsCounts: List<Pair<Int, Int>> = allQueensResultsCounts
 }
